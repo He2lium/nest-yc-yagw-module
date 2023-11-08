968cc26c29c6da0832d698a3a5d5110ed6c161a3
he2lium <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>
2023-11-08 09:52:25 +0700

    auth roles

--- lib/types/scope.type.ts
+++ lib/types/scope.type.ts
@@ -1,14 +1,35 @@
 /**
  * Example: AuthRoleType<typeof UserRole, Scopes>
  */
-export type AuthRoleType<RoleEnum, ScopeEnum> = { [ key in keyof RoleEnum ]: Array<ScopeEnum> }
+export type AuthRoleType<RoleEnum, ScopeEnum> = { [key in keyof RoleEnum]: Array<ScopeEnum> }
 
-export const AuthRoleMerger = (AuthRole: {[key: string]: string}) => (roles: string[], toString: boolean = false): string[] | string => {
-    let scopes: string[] = []
-    for (let role of roles) {
-        scopes = [...scopes, ...AuthRole[role]]
+export const AuthRoleMerger = <AuthRoleType>(AuthRole: AuthRoleType) =>
+    (roles: string[], toString: boolean = false): string[] | string => {
+        let scopes: string[] = []
+        for (let role of roles) {
+            scopes = [...scopes, ...AuthRole[role]]
+        }
+        const set = [...new Set(scopes)]
+
+        return toString ? set.join(' ') : set
     }
-    const set = [...new Set(scopes)]
 
-    return toString ? set.join(' ') : set
+
+export enum UserRole {
+    user = 'user',
+    administrator = 'administrator',
+    moderator = 'moderator'
 }
+
+export enum Scopes {
+    moderatingRealEstate = 're:mo',
+    moderatingTenant = 're:te',
+    moderatingWarehouse = 're:wh',
+}
+
+const AuthRole: AuthRoleType<typeof UserRole, Scopes> = {
+    user: [],
+    administrator: Object.values(Scopes),
+    moderator: Object.values(Scopes)
+}
+export const RoleMerger1 = AuthRoleMerger<typeof AuthRole>(AuthRole)
